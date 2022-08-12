3c261de90d48b69311c3e175503e4c8a8fbd47bd
hllcncngz <hllcncngz1@gmail.com>
2022-08-12 16:27:15 +0300

    middleware

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const fn = () => {
-    console.log("Çalıştı")
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const myLogger = (req, res, next) => {
+    console.log("Middleware 1");
+    next();
 }
+
+const myLogger2 = (req, res, next) => {
+    console.log("Middleware 2");
+    next();
+}
+
+app.use(express.static("public"))
+app.use(myLogger)
+app.use(myLogger2)
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "temp/index.html"))
+})
+
+const PORT = 3002
+app.listen(PORT, () => {
+    console.log(`sunucu ${PORT} portundan başlatıldı`);
+})
